2943933fcfd49a0d0dc16ede7f7d453c50ac19bf
Carlos Lopes <clfreitaas18@gmail.com>
2026-04-10 15:22:53 -0400

    8º commit: Editando aula 4 (BREAK e CONTINUE)

--- aula4.py
+++ aula4.py
@@ -19,6 +19,25 @@ for i in range(1, 11, 1):
     print(i, " ", end="")
 print("\n------------------------------------------------\n")
 
+# COMANDOS BREAK E CONTINUE ------------------------------------------------------
+
+print("----- CONTAGEM DE 1 A 10 COM FOR (BREAK / CONTINUE) -----")
+
+for i in range(1, 11, 1):
+    if i == 5:
+        continue
+    print(i, " ", end="")
+print("(continue)\n")
+# ignora o comando quando ele é 5 e segue a repetição
+
+for i in range(1, 11, 1):
+    if i == 5:
+        break
+    print(i, " ", end="")
+print("(break)")
+# quebra a estrutura de repetição e segue para o próximo comando
+print("-------------------------------------------------------\n")
+
 # QUANDO USAR WHILE OU FOR/RANGE
 # 'while' não sei quantas vezes o loop deve repetir, deve parar quando atender uma condição específica
 # 'for/range' sei exatamente quantas vezes o loop deve repetir
@@ -34,7 +53,7 @@ while opc == 1:
     print("0 - SAIR")
     opc2 = int(input(":"))
 
-    while opc2 == 1:
+    for opc2 in range(1, 6, 1):
         divi = float()
         n1 = int(input("\nDigite um numero: "))
         n2 = int(input("Digite outro numero: "))
@@ -48,8 +67,11 @@ while opc == 1:
            f"Subtracao: {sub}\n"
            f"Multiplicacao: {multi}\n"
            f"DIvisao: {divi: .2f}")
+        
         opc2 = int(input("\n1 - continuar\n"
                          "0 - sair: "))
+        if opc2 == 0:
+            break
     
     opc = int(input("\n1 - Retornar ao menu\n"
                     "0 - Sair: "))
